615fc6c86fab4540e6ea0377218904c1bda7fcde
4quet <louis.fourquet@gmail.com>
2017-04-04 15:22:08 +0200

    Import from nuwa

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # standard-settings
 Standardised settings loader
+
+Note: First settings loaded are stronger than last one  
+## Priority order
+### env
+`$ SERVER_PORT=2500 node index.js` or `$ service_spacebro_inputMessage=new-media node index.js`  
+
+### argv
+`$ node index.js --server.port 2000`  
+
+### settings file with argv
+`$ node index.js --settings settings/settings.prod.json`  
+
+### settings files
+`settings/settings.json`  
+`settings/settings.default.json`
---
+++ index.js
@@ -0,0 +1,35 @@
+// first settings loaded are stronger than last one
+// Priority order:
+// env : `$ SERVER_PORT=2500 node index.js` or `$ service_spacebro_inputMessage=new-media node index.js`
+// argv: `$ node index.js --server.port 2000`
+// settings file with argv: `$ node index.js --settings settings/settings.prod.json`
+// settings file located at `settings/settings.json`
+// settings file located at `settings/settings.default.json`
+
+// TODO: load settings in data too
+
+'use strict'
+
+const nconf = require('nconf')
+const fs = require('fs')
+
+function load () {
+  nconf.env({
+    lowerCase: true,
+    separator: '_'
+  })
+  .argv()
+
+  if (nconf.get('settings') !== undefined) {
+    if (!fs.existsSync(nconf.get('settings'))) {
+      console.warn('File ' + nconf.get('settings') + ' does not exist and was not loaded. Other settings applied.')
+    } else {
+      nconf.file('cli', nconf.get('settings'))
+    }
+  }
+
+  nconf.file({ file: './settings/settings.json' })
+  .file('default', './settings/settings.default.json')
+}
+
+module.exports = { load }
